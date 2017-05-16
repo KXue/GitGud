fd9378dc2209569d3b6fc21cb1cdf5e4dba993b1
Kevin Xue <ivexnuke@gmail.com>
2017-05-15 21:16:20 -0400

    Changed renderable to take in a paint and canvas

--- app/src/main/java/com/gud/git/gitgud/Engine/Renderable.java
+++ app/src/main/java/com/gud/git/gitgud/Engine/Renderable.java
@@ -1,9 +1,12 @@
 package com.gud.git.gitgud.Engine;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 /**
  * Created by KevinXue on 5/15/2017.
  */
 
 public interface Renderable {
-    void onDraw();
+    void onDraw(Paint paint, Canvas canvas);
 }
